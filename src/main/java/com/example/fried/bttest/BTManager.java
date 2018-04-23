0ff458094f708e6be6c64f2c8c929f1737b719bf
afriedman12 <alexia@cognosis.com>
2018-04-22 18:48:01 -0700

    Add files via upload

---
+++ src/main/java/com/example/fried/bttest/BTManager.java
@@ -0,0 +1,82 @@
+package com.example.fried.bttest;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+
+public class BTManager {
+    private static BluetoothAdapter btAdapter;
+    private static BluetoothSocket btSocket;
+    private static OutputStream btOutStream;
+
+    private static String LOG_TAG = "BTManager";
+
+    private static String TARGET_DEVICE = "C4:9D:ED:06:39:9D";
+
+    // Request range : 5000 - 5999
+    private static int REQUEST_ENABLE_BT = 5001;
+
+    public static BluetoothDevice init(Activity activity) {
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
+            // TBD : need to handle bluetooth being disabled or unavailable
+        }
+
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        BluetoothDevice btServer = null;
+
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress();
+
+                if (deviceHardwareAddress.equals(TARGET_DEVICE))
+                    btServer = device;
+            }
+        }
+
+        return btServer;
+    }
+
+    public static synchronized void SetBluetoothSocket(BluetoothSocket newBtSocket) {
+
+        // null out the existing socket and stream if they exist
+        btSocket = null;
+        btOutStream = null;
+
+        if (newBtSocket != null) {
+            try {
+                OutputStream btStream = newBtSocket.getOutputStream();
+
+                btSocket = newBtSocket;
+                btOutStream = btStream;
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error creating output stream", e);
+            }
+        }
+    }
+
+    public static synchronized void WriteToSocket(String s) {
+        if (btSocket == null) {
+            Log.e(LOG_TAG, "Attempted to write without an open stream");
+        } else {
+            try {
+                btOutStream.write(s.getBytes());
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Failed to write to output stream");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/fried/bttest/ConnectThread.java
@@ -0,0 +1,77 @@
+package com.example.fried.bttest;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
+class ConnectThread extends Thread {
+    private final BluetoothSocket socket;
+    private final BluetoothDevice device;
+
+    private final String LOG_TAG = "ConnectThread";
+
+
+    private final UUID MY_UUID = UUID.fromString("633e589e-2036-4fac-8371-d9a9544e9355");
+
+    public ConnectThread(BluetoothDevice connectToDevice) {
+        // Use a temp object that is later assigned to the class member
+        // because the class member is final
+        BluetoothSocket tmpSocket = null;
+        device = connectToDevice;
+
+        try {
+            // Get a BluetoothSocket to connect with the given BluetoothDevice.
+            // MY_UUID is the app's UUID string, also in the server code
+            tmpSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            Log.e (LOG_TAG, "Socket's create method failed", e);
+        }
+
+        socket = tmpSocket;
+    }
+
+    public void run() {
+        try {
+            // Connect to the remote device through the socket. This call blocks until
+            // it succeeds or throws an exception
+            socket.connect();
+        } catch (IOException connectException) {
+            // unable to connect...close the socket and return
+            try {
+                socket.close();
+            } catch (IOException closeException) {
+                Log.e(LOG_TAG, "Could not close the client socket", closeException);
+            }
+
+            return;
+        }
+
+        manageMyConnectedSocket(socket);
+    }
+
+    public void cancel() {
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Could not close the client socket", e);
+        }
+    }
+
+    private void manageMyConnectedSocket(BluetoothSocket localSocket)
+    {
+        BTManager.SetBluetoothSocket(localSocket);
+        BTManager.WriteToSocket("up\r\n");
+        BTManager.WriteToSocket("up\r\n");
+        BTManager.WriteToSocket("down\r\n");
+    }
+
+
+
+
+
+}
